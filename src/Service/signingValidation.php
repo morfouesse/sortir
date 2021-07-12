2df57474da177c1d87cb7ca5047b60743d7861ad
Martin F <martin.flechard2021@campus-eni.fr>
2021-07-12 17:05:17 +0200

    front v2

--- src/Service/signingValidation.php
+++ src/Service/signingValidation.php
@@ -6,8 +6,17 @@ use App\Entity\Activity;
 
 class signingValidation{
 
-    public function canSignIn(Activity $activity){
+    public function canSignIn(Activity $activity): bool{
+        //To test before sign the user in : state : open | limitDate ok | limitInscriptions ok
+        $stateIsOpen = $activity->getState()->getLabel() == 'open';
+        $limitDateNotPassed = $activity->getInscriptionLimitDate() > new \DateTime('now');
+        $activityIsNotFull = count($activity->getUsers()) < $activity->getMaxInscriptionsNb();
 
+        if ($stateIsOpen && $limitDateNotPassed && $activityIsNotFull){
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
